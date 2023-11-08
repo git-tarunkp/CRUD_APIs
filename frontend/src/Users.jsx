0ef8be88958b5662fee348fdbc2746550bc253f9
TARUN <108211464+git-tarunkp@users.noreply.github.com>
2023-11-08 20:11:09 +0530

    Update Users.jsx

--- frontend/src/Users.jsx
+++ frontend/src/Users.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 function Users() {
 
+        axios.defaults.withCredentials=true;
+
         const [users,setUsers]=useState([])
 
         useEffect(()=>{
