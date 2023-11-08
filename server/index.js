a876a284527829a7112aa9d36abc88c68599cc84
TARUN <108211464+git-tarunkp@users.noreply.github.com>
2023-11-08 13:28:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors(
     
 {
-    origin:["http://crud-ap-frontend-9fbvmwxh1-taruns-projects-f750e9da.vercel.app"],
+    origin:["https://crud-ap-frontend-9fbvmwxh1-taruns-projects-f750e9da.vercel.app"],
     methods:["POST","GET","DELETE","PUT"],
     credentials:true
 
