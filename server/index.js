e923f8b75e799f54fb8ef4520039d86382639182
TARUN <108211464+git-tarunkp@users.noreply.github.com>
2023-12-18 16:06:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,8 +8,7 @@ app.use(cors(
 {
     origin:["https://crud-ap-frontend.vercel.app/**"],
    
-    methods:["POST","GET","DELETE","PUT"],
-    credentials:true
+    methods:["POST","GET","DELETE","PUT"]
 
     // origin: 'https://crud-ap-frontend.vercel.app',
     // optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
