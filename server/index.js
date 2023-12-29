5ac9621345ca50727f48556da3fa9b954da048cc
TARUN <108211464+git-tarunkp@users.noreply.github.com>
2023-12-29 19:00:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors(
     ))
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://metarun:Tarunpatel@cluster0.jg0pnvi.mongodb.net/crud?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://metarun:metarun@cluster0.jg0pnvi.mongodb.net/crud?retryWrites=true&w=majority');
 
 
 app.get('/', (req, res) => {
